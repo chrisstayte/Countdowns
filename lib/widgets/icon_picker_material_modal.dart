54f466c1ffff9b1e4f1159af83f5f3165c29b0a0
Chris <ctstayte@gmaill.com>
2022-01-07 18:43:56 -0500

    backup

---
+++ lib/widgets/icon_picker_material_modal.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class IconPickerMaterialModal extends StatefulWidget {
+  const IconPickerMaterialModal({Key? key}) : super(key: key);
+
+  @override
+  _IconPickerMaterialModalState createState() =>
+      _IconPickerMaterialModalState();
+}
+
+class _IconPickerMaterialModalState extends State<IconPickerMaterialModal> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
