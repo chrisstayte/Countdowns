1dc510eeeb49eac89633a380bd563c919d5ec294
Chris Stayte <ctstayte@gmail.com>
2025-02-16 19:44:52 -0500

    Add app route constants
    Created a new file for app route constants including paths for home, event details, settings, and actions like edit and new.

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  static String home = '/';
+  static String event = 'event/:id';
+  static String settings = 'settings';
+  static String appIcon = 'appIcon';
+  static String eventEdit = 'edit';
+  static String eventNew = 'new';
+}
